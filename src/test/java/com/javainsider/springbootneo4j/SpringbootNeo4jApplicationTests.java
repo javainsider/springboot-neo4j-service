72007989557eafc29b8b4b24380a028ec0d09c59
Rudra Garnaik <rudra4all@gmail.com>
2019-12-03 20:46:00 -0500

    initial code commit

---
+++ src/test/java/com/javainsider/springbootneo4j/SpringbootNeo4jApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javainsider.springbootneo4j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootNeo4jApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
